7a6c7313ff7b328ba7325a0b72a8f2e0571c4a43
xuuwon <dumpling1011@gmail.com>
2024-10-10 18:53:54 +0900

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+// import Hello from './component/Hello'
+import Project from './component/Project'
+
+function App() {
+  return (
+    <Project/>
+  )
+}
+
+export default App
